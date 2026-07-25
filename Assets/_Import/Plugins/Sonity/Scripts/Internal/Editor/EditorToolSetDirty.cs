4467d50ec752093413ca52cf2f2114dc8ff0719c
Fouad Elamoury <foad.osama@hotmail.com>
2026-07-26 02:06:54 +0300

    adding main menu and gameplay music

---
+++ Assets/_Import/Plugins/Sonity/Scripts/Internal/Editor/EditorToolSetDirty.cs
@@ -0,0 +1,21 @@
+// Created by Victor Engström
+// Copyright 2024 Sonigon AB
+// http://www.sonity.org/
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+namespace Sonity.Internal {
+
+    public static class ToolsSetDirty {
+
+        [MenuItem("Tools/Sonity ??/Set Selected Assets as Dirty (Force Reserialize for Resave)")]
+        private static void SetDirty() {
+            foreach (UnityEngine.Object objects in Selection.objects) {
+                EditorUtility.SetDirty(objects);
+            }
+        }
+    }
+}
+#endif
---
+++ Assets/_Import/Plugins/Sonity/Scripts/Public/ScriptableObjects/SoundPreset.cs
@@ -0,0 +1,26 @@
+// Created by Victor Engström
+// Copyright 2024 Sonigon AB
+// http://www.sonity.org/
+
+using UnityEngine;
+using System;
+using Sonity.Internal;
+
+#if UNITY_EDITOR
+
+namespace Sonity {
+
+    /// <summary>
+    /// Use <see cref="SoundPresetBase">SoundPresets</see> to apply settings to <see cref="SoundContainerBase">SoundContainers</see> and <see cref="SoundEventBase">SoundEvents</see> automatically by matching the names of your assets.
+    /// Its matched and applied when you Create Assets From Selection or manually from the <see cref="SoundContainerBase">SoundContainer</see> Presets and the <see cref="SoundEventBase">SoundEvent</see> Presets toolbars.
+    /// Example use: Name UI sounds to “UI_” and use Is Prefix “UI_” to automatically assign settings like disabling distance and setting Spatial Blend to 0.
+    /// The settings are applied ordering from the one highest up and going downwards and can match an asset multiple times overwriting previous values.
+    /// You can have multiple <see cref="SoundPresetBase">SoundPreset</see> objects.
+    /// </summary>
+    [Serializable]
+    [CreateAssetMenu(fileName = "SoundPreset_", menuName = "Sonity ??/SoundPreset", order = 300)] // Having a big gap in indexes creates dividers
+    public class SoundPreset : SoundPresetBase {
+
+    }
+}
+#endif
